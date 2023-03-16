61886c7a52a9ebe4586c70d76dc20d267689832c
Igor Pol <igor.pol@gfx.dom>
2023-03-16 16:52:35 +0300

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const accessButton = document.querySelector("#button");
+const accessEmail = document.querySelector("#email");
+const accessAlert = document.querySelector(".main__early-access--invisible");
+
+accessButton.addEventListener("click", () => {
+    if (!(accessEmail.value.includes("@") & accessEmail.value.includes("."))) {
+        accessAlert.style.display = "block";
+    } else {
+        accessAlert.style.display = "none";
+    }
+})
